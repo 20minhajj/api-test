d13b2ec745e26aefec2e350602005590fe6e12f1
20minhajj <harith20@icloud.com>
2019-07-02 17:42:43 +0300

    feat: exporting the replayer middleware

---
+++ controllers/sms.js
@@ -0,0 +1,54 @@
+const Sms = require('../model/sms');
+
+exports.createSms = (req, res, next) => {
+  req.body.sms = JSON.parse(req.body.sms);
+  const txt = new Sms({
+
+    userId: "req.body.txt.userId",
+    senderNumber: "req.body.txt.senderNumber",
+    name: "req.body.txt.name",
+    description: "req.body.txt.description",
+
+  });
+
+  txt.save().then(
+    () => {
+      res.status(201).json({
+        message: 'Sms deliverd Successfully!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+}
+
+exports.respondSms = (req, res, next) => {
+  req.body.sms = JSON.parse(req.body.sms);
+  const txt = new Sms({
+
+    userId: req.body.txt.userId,
+    senderNumber: req.body.txt.senderNumber,
+    name: req.body.txt.name,
+    description: req.body.txt.description,
+
+  });
+
+  txt.save().then(
+    () => {
+      res.status(201).json({
+        message: 'Sms replayed Successfully!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: "Sms not Successfully replayed"
+      });
+    }
+  );
+}
+
