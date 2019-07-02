ac7920bf15bd8fe5ba6d5bbdcb897e6c28f46581
20minhajj <harith20@icloud.com>
2019-07-02 18:04:15 +0300

    chore: installing mongoose in the app

--- routes/sms.js
+++ routes/sms.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const smsCtrl = require('../controllers/sms');
 
 router.get('/', smsCtrl.createSms);
-router.get('/respond', smsCtrl.respondSms);
+router.post('/respond', smsCtrl.respondSms);
 
 
 module.exports = router;
